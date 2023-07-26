58d1413cd53f947ddf55b04820b4b390d7f823a7
plachert <pmlachert@gmail.com>
2023-07-26 15:35:16 +0200

    Refactor

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -14,23 +14,19 @@ class ImageNet100(Dataset):
         self.path = path
         self.train_or_val = "train" if is_train else "val"
         self.image_paths = self._get_image_paths()
-        self.labels = self._load_labels()
-        self.new_label_mapper = self._create_mapping() # maps original id to a number (0-99)   
+        self.labels = self._create_mapping() # maps original id to a number (0-99)   
         
     def _get_image_paths(self):
         image_paths = list(self.path.glob(f'{self.train_or_val}*/*/*.JPEG'))
         return image_paths
             
-    def _load_labels(self):
-        with open(self.path.joinpath("Labels.json"), "r") as file:
-            labels = json.load(file)
-        return labels
-    
     def _create_mapping(self):
         mapping = {}
-        for new_id, (id_, class_name) in enumerate(self.labels.items()):
-            mapping[id_] = new_id
-        return mapping
+        with open(self.path.joinpath("Labels.json"), "r") as file:
+            labels = json.load(file)
+        for idx, (id_, class_name) in enumerate(labels.items()):
+            mapping[id_] = (idx, class_name)
+        return mapping        
         
     def __len__(self):
         return len(self.image_paths)
@@ -39,8 +35,8 @@ class ImageNet100(Dataset):
         image_path = self.image_paths[idx]
         image = np.array(Image.open(image_path))
         id_ = image_path.parent.name
-        new_id = self.new_label_mapper[id_]
-        return image, new_id
+        class_idx, class_name = self.labels[id_]
+        return {"image": image, "class_idx": class_idx, "class_name": class_name}
         
 if __name__ == "__main__":
     ds = ImageNet100(pathlib.Path('/home/piotr/datasets/vision/imagenet_100'), is_train=False)
