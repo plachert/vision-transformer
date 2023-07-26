f5a1db39b0f2996d833a8041e767949b2f352dd9
plachert <pmlachert@gmail.com>
2023-07-26 15:24:25 +0200

    Add new mapping and getitem

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -2,6 +2,7 @@ from torch.utils.data import Dataset
 import pathlib
 from PIL import Image
 import json
+import numpy as np
 
 
 class ImageNet100(Dataset):
@@ -12,8 +13,9 @@ class ImageNet100(Dataset):
         super().__init__()
         self.path = path
         self.train_or_val = "train" if is_train else "val"
-        self.labels = self._load_labels()
         self.image_paths = self._get_image_paths()
+        self.labels = self._load_labels()
+        self.new_label_mapper = self._create_mapping() # maps original id to a number (0-99)   
         
     def _get_image_paths(self):
         image_paths = list(self.path.glob(f'{self.train_or_val}*/*/*.JPEG'))
@@ -23,13 +25,23 @@ class ImageNet100(Dataset):
         with open(self.path.joinpath("Labels.json"), "r") as file:
             labels = json.load(file)
         return labels
+    
+    def _create_mapping(self):
+        mapping = {}
+        for new_id, (id_, class_name) in enumerate(self.labels.items()):
+            mapping[id_] = new_id
+        return mapping
         
     def __len__(self):
-        pass
+        return len(self.image_paths)
     
     def __getitem__(self, idx):
-        pass
+        image_path = self.image_paths[idx]
+        image = np.array(Image.open(image_path))
+        id_ = image_path.parent.name
+        new_id = self.new_label_mapper[id_]
+        return image, new_id
         
 if __name__ == "__main__":
     ds = ImageNet100(pathlib.Path('/home/piotr/datasets/vision/imagenet_100'), is_train=False)
-    print(ds.image_paths)
+    print(ds[0])
