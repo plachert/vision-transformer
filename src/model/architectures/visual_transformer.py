772b090b5cbb3eb7241a5511590fb821e9ef29f0
plachert <pmlachert@gmail.com>
2023-08-10 13:48:23 +0200

    Add building blocks

---
+++ src/model/architectures/visual_transformer.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.Module):
+    pass
+
+
+class ScaledDotProductAttention(nn.Module):
+    pass
+
+
+class MultiHeadAttention(nn.Module):
+    pass
+
+
+class TransformerEncoder(nn.Module):
+    pass
+
+
+class ViT(nn.Module):
+    pass
