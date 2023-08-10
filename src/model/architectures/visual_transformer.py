2e3c175b5178e038ba0cb323af26f8ea2e328b07
plachert <pmlachert@gmail.com>
2023-08-10 14:35:06 +0200

    Complete scaled dot-product attention

--- src/model/architectures/visual_transformer.py
+++ src/model/architectures/visual_transformer.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
 
 
 class PositionalEncoding(nn.Module):
@@ -13,9 +14,11 @@ class ScaledDotProductAttention(nn.Module):
         
     def forward(self, q, k, v):
         d_k = q.shape[-1]
-        attention_scores = torch.matmul(q, k.transpose(-2, -1))
-        attention_scores_scaled = attention_scores / torch.sqrt(torch.tensor(d_k))
-        return attention_scores 
+        attention_scores = torch.matmul(q, k.transpose(-2, -1)) # matmul Q, K
+        attention_scores_scaled = attention_scores / torch.sqrt(torch.tensor(d_k)) # Scale
+        attention_weights  = F.softmax(attention_scores_scaled, -1) # softmax
+        attention = torch.matmul(attention_weights, v) # matmul weights and V
+        return attention 
 
 
 class MultiHeadAttention(nn.Module):
@@ -32,5 +35,6 @@ class ViT(nn.Module):
 
 if __name__ == "__main__":
     a = ScaledDotProductAttention()
-    q, k, v = [torch.rand(1, 100, 128) for _ in range(3)]
-    print(a(q, k, v).shape)
+    q, k, v = [torch.rand(1, 3, 1) for _ in range(3)]
+    print(q, k, v)
+    print(a(q, k, v))
