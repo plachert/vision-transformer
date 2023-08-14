5bd9a2efd1f910d9f2b49df940054143916c70f6
plachert <pmlachert@gmail.com>
2023-08-14 16:06:47 +0200

    Add CIFAR10 datamodule

--- src/data/datamodules.py
+++ src/data/datamodules.py
@@ -1,5 +1,7 @@
 from src.data.base_datamodule import BaseDataModule
-from src.data.datasets import ImageNet100
+from src.data.datasets import ImageNet100, CIFAR10
+import torch
+import lightning as L
 
 
 class ImageNet100DataModule(BaseDataModule):
@@ -16,3 +18,21 @@ class ImageNet100DataModule(BaseDataModule):
             is_train=False,
             )
         self.test_ds = None
+        
+        
+class CIFAR10DataModule(BaseDataModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def setup(self, stage: str):
+        """Split based on https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial15/Vision_Transformer.html"""
+        if stage == "fit":
+            train_ds = CIFAR10(train=True, transform=self.train_transform)
+            val_ds = CIFAR10(train=True, transform=self.inference_transform)
+            L.seed_everything(42)
+            self.train_ds, _ = torch.utils.data.random_split(train_ds, [45000, 5000])
+            L.seed_everything(42)
+            _, self.val_ds = torch.utils.data.random_split(val_ds, [45000, 5000])
+        else:
+            self.test_ds = CIFAR10(train=False, transform=self.inference_transform)
+
