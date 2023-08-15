7092aafa6716e3963b9467ba5af9e3d6787aa594
plachert <pmlachert@gmail.com>
2023-08-15 17:10:17 +0200

    Add training script

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 from model.classifier import ImageClassifier
-from src.data.datamodules import ImageNet100DataModule
+from src.data.datamodules import ImageNet100DataModule, CIFAR10DataModule
 from torchvision import models, transforms
 import cv2
 from functools import partial
@@ -8,34 +8,68 @@ from torch.optim import Adam
 import torch.nn as nn
 from lightning.pytorch import loggers as pl_loggers
 import numpy as np
+from src.data.transform import Patchify
+from src.model.architectures.visual_transformer import VisionTransformer
 
 
-train_transform = transforms.Compose([
-    transforms.Resize(224),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
-])
-inference_transform = train_transform
+inference_transform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.CenterCrop(224),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        Patchify((16, 16)),
+    ]
+    )
+train_transform = transforms.Compose(
+    [
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomResizedCrop((224, 224), scale=(0.8,1.0), ratio=(0.9,1.1)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+        Patchify((16, 16)),
+    ]
+    )
+
 
 def main():    
+    # datamodule = CIFAR10DataModule(
+    #     train_batch_size=128,
+    #     val_batch_size=128,
+    #     train_transform=train_transform, 
+    #     inference_transform=inference_transform
+    # )
     datamodule = ImageNet100DataModule(
-        train_transform=train_transform, inference_transform=inference_transform,
+        train_batch_size=128,
+        val_batch_size=128,
+        train_transform=train_transform, 
+        inference_transform=inference_transform
     )
-    m = models.mobilenet_v2(pretrained=True)
-    for param in list(m.features.parameters()):
-        param.requires_grad = False
-    m.classifier = nn.Sequential(
-    nn.Dropout(0.2),
-    nn.Linear(1280, 100)
-)
+    
+    #####
+#     m = models.mobilenet_v2(pretrained=True)
+#     for param in list(m.features.parameters()):
+#         param.requires_grad = False
+#     m.classifier = nn.Sequential(
+#     nn.Dropout(0.2),
+#     nn.Linear(1280, 100)
+# )
+    m = VisionTransformer(
+        emb_dim=256, 
+        flatten_patch_dim=16*16*3, 
+        num_blocks=6, 
+        num_heads=8, 
+        num_patches=196, 
+        num_classes=100, 
+        dim_feedforward=512,
+        dropout=0.2,
+        )
+    #####
     model = ImageClassifier(
         model=m,
         optimizer_factory=partial(Adam, lr=0.001),
         num_classes=100,
         )
-    tb_logger = pl_loggers.TensorBoardLogger(save_dir='logs/')
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir='logs/imagenet100_vit_16')
     trainer = L.Trainer(max_epochs=100, accelerator='gpu', logger=tb_logger)
     trainer.fit(model, datamodule)
 
